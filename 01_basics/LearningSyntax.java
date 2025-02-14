e3c4ca908eec420229eb5b26fe2cd7ef7c190282
Rumit Shah <rumitshah02@gmail.com>
2025-02-14 21:12:35 +0000

    Learned basics, history, difference between JDK vs JRE vs JVM, how the code compiles and syntax.

---
+++ 01_basics/LearningSyntax.java
@@ -0,0 +1,7 @@
+public class LearningSyntax {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Good morning");
+        System.out.println("Subscribed to Java");
+    }
+}
