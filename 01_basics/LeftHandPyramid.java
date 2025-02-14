13d391a74a987f3d5bf80d2fc5fd3cd765cd442e
Rumit Shah <rumitshah02@gmail.com>
2025-02-15 02:43:55 +0530

    Add files via upload
    Learned basics, history, difference between JDK vs JRE vs JVM, how the code compiles and syntax.

---
+++ 01_basics/LeftHandPyramid.java
@@ -0,0 +1,9 @@
+public class LeftHandPyramid {
+    public static void main(String[] args) {
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * * ");
+        System.out.println("* * * * *");
+    }
+}
