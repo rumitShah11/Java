ee1b40ffd149a8e487556d5201d04c8af7f2672e
Rumit Shah <rumitshah02@gmail.com>
2025-02-20 22:55:10 +0530

    learned about data types and user input.

---
+++ 02_dataTypes&Variables/Variables.java
@@ -0,0 +1,38 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber  /*DECLARATION OF VARIABLE*/ = 865; // INITIALIZATION OF VARIABLE
+        int myNUMBER = 865;
+        int yourNumber = 0;
+
+        int asdf987 = 8;
+
+        int uh87d87yyd87 = 90;
+
+        System.out.println(yourNumber);
+        yourNumber = 865;
+        System.out.println(yourNumber);
+        yourNumber = 45;
+        System.out.println(yourNumber);
+
+        float myFloat = 5.0f;
+        double myDouble = 6.98766;
+        System.out.println(myFloat);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+
+        String wishes = "Good Morning";    // Double Inverted Comma is Used for String Data type.
+        System.out.println(wishes);
+
+        char myCharacter = 'K';   //Single Inverted Comma Used for char Data type
+
+        /*
+            Datatype        variableName(Identifier) =  Liberals
+
+            If Datatype is String -> String Literals
+            If Datatype id int -> int Literal
+            If Datatype is boolean -> Boolean Literal
+        */
+
+    }
+}
