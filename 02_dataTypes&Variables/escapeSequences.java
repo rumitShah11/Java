a2592284a6c23e914279f216ee4bc4c080730ba2
Rumit Shah <rumitshah02@gmail.com>
2025-02-25 23:51:05 +0530

    escape sequences (\n, \t, \\)

---
+++ 02_dataTypes&Variables/escapeSequences.java
@@ -0,0 +1,34 @@
+public class escapeSequences {
+    public static void main(String[] args) {
+
+        System.out.println("Right Half Pyramid Pattern");
+        System.out.println(" *\n *\t*\n *\t*\t*\n *\t*\t*\t*\n *\t*\t*\t*\t*\n");
+
+        // Right Half Pyramid Pattern
+        // *
+        // *	*
+        // *	*	*
+        // *	*	*	*
+        // *	*	*	*	*
+
+        System.out.println("Reverse Right Half Pyramid Pattern");
+        System.out.println(" *\t*\t*\t*\t*\n *\t*\t*\t*\n *\t*\t*\n *\t*\n *\n");
+
+//        Reverse Right Half Pyramid Pattern
+//                *	*	*	*	*
+//                *	*	*	*
+//                *	*	*
+//                *	*
+//                *
+
+        System.out.println("Left Half Pyramid Pattern");
+        System.out.println("\t\t\t\t*\n \t\t\t*\t*\n \t\t*\t*\t*\n \t*\t*\t*\t*\n *\t*\t*\t*\t*\n");
+
+//        Left Half Pyramid Pattern
+//				    *
+// 			    *	*
+// 		    *	*	*
+// 	    *	*	*	*
+//  *	*	*	*	*
+    }
+}
