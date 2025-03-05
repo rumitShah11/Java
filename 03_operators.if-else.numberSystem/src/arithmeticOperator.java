b23ada1cf604216b05026793bead21df730720f3
Rumit Shah <rumitshah02@gmail.com>
2025-03-05 17:48:14 +0530

    +,-,*,/,%

---
+++ 03_operators.if-else.numberSystem/src/arithmeticOperator.java
@@ -0,0 +1,20 @@
+public class arithmeticOperator {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        System.out.println(a+b);   //13
+        System.out.println(a-b);   //3
+        System.out.println(a*b);   //40
+        System.out.println(a/b);   //1
+        System.out.println(a%b);   //3
+
+        double x = 5;
+        double y = 3;
+        System.out.print("\n");
+        System.out.println(x+y);   //8.0
+        System.out.println(x-y);   //2.0
+        System.out.println(x*y);   //15.0
+        System.out.println(x/y);   //1.66666
+    }
+}
